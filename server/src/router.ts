990fc83dbd8b64cb9b0023a904e06c88cae224fa
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-22 11:04:53 +0100

    [FEAT] : create services to help token creation and back auth + complete auth for register and login

--- server/src/router.ts
+++ server/src/router.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post("/auth/register", authActions.register);
 router.post("/auth/login", checkAuthDatas, authActions.login);
+router.get("/auth/find/:id", authActions.findCurrentUser);
 
 export default router;
