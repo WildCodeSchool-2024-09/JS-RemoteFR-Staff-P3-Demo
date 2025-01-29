c570ef1a74021d9441829a00696eace9fd3dd404
Benoît Vandanjon <benoit@vandanjon.fr>
2025-01-29 12:12:20 +0100

    [FEAT] : add avatar asset + import in Profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
+import profilPicture from "../assets/images/profil_neutral.webp";
+
 function Profile() {
   const { user, logOut } = useAuth();
 
@@ -14,6 +16,8 @@ function Profile() {
   return (
     <div id="ProfileContainer">
       <h1>Profil de {user ? user.username : "inconnu"}</h1>
+
+      <img src={profilPicture} alt={user ? user.username : "inconnu"} />
 
       <p>Email : {user ? user.email : "inconnu"}</p>
 
